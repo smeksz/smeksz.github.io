bb3eb3ac5529d13d680360c23675f287b9b3dc3e
smeksz <96016162+smeksz@users.noreply.github.com>
2024-05-30 13:27:37 -0400

    Create tac.js

---
+++ tac.js
@@ -0,0 +1,6 @@
+let questionTXT
+
+document.getElementById('submitQA').addEventListener("click", function() {
+    questionTXT = document.getElementById('question');
+    console.log(questionTXT)
+});
