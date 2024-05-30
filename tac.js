3ee9a7d37f4bb8d47f17aabc116c8b94a0dc8985
smeksz <96016162+smeksz@users.noreply.github.com>
2024-05-30 13:35:14 -0400

    Update tac.js

--- tac.js
+++ tac.js
@@ -1,5 +1,7 @@
 let questionTXT
 
+console.log("HELLO")
+
 document.getElementById('submitQA').addEventListener("click", function() {
     questionTXT = document.getElementById('question');
     console.log(questionTXT)
